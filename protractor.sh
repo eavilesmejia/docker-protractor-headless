b0db08ad779f924abfa298619923e96fed174027
Edgar Aviles <eavilesmejia@gmail.com>
2017-01-06 09:39:43 -0600

    running selenium server

--- protractor.sh
+++ protractor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xvfb-run -a --server-args='-screen 0 1280x1024x24' protractor $@
+webdriver-manager start --detach && xvfb-run -a --server-args='-screen 0 1280x1024x24' protractor $@
 
