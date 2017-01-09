f8b73438686160e4905df5529a106f51d6e1fcfb
Edgar Aviles <eavilesmejia@gmail.com>
2017-01-09 12:30:51 -0600

    removing start selenium server

--- protractor.sh
+++ protractor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-webdriver-manager start --detach && xvfb-run -a --server-args='-screen 0 1280x1024x24' protractor $@
+xvfb-run -a --server-args='-screen 0 1280x1024x24' protractor $@
 
