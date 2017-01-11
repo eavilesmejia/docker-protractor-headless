af800d89ecca68b421c93adaa37543c4421644bb
Edgar Aviles <eavilesmejia@gmail.com>
2017-01-11 11:42:44 -0600

    Update protractor.sh

--- protractor.sh
+++ protractor.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-export DISPLAY=:1.0
-Xvfb :0 -ac -screen 0 1024x768x24 &
-webdriver-manager start --detach 
-protractor $@
+xvfb-run -a --server-args='-screen 0 1280x1024x24' protractor $@
 
